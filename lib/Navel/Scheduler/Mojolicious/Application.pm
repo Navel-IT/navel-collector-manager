eb0ff18f018120091cfa4ab5bc6c0d39c9d76589
le-garff-yoann <pe.weeble@yahoo.fr>
2016-01-11 01:47:47 +0100

    remove exception template -> ok_ko() instead

--- lib/Navel/Scheduler/Mojolicious/Application.pm
+++ lib/Navel/Scheduler/Mojolicious/Application.pm
@@ -91,14 +91,26 @@ sub startup {
                             my $exception = delete $controller->stash()->{exception};
 
                             if (defined $exception) {
-                                $controller->scheduler()->{core}->{logger}->error(
-                                    $controller->scheduler()->{core}->{logger}->stepped_log(
-                                        [
-                                            'an exception has been raised by Mojolicious for HTTP ' . $controller->req()->method() . ' on ' . $controller->req()->url()->to_string() . ': ',
-                                            $exception
-                                        ]
-                                    )
+                                my @exception_message = (
+                                    'an exception has been raised for HTTP ' . $controller->req()->method() . ' on ' . $controller->req()->url()->to_string() . ': ',
+                                    $exception
                                 );
+
+                                $controller->scheduler()->{core}->{logger}->error(
+                                    $controller->scheduler()->{core}->{logger}->stepped_log(\@exception_message)
+                                );
+
+                                $controller->render(
+                                    json => $controller->ok_ko(
+                                        {
+                                            ok => [],
+                                            ko => @exception_message
+                                        }
+                                    ),
+                                    status => 500
+                                );
+
+                                return undef;
                             }
                         }
                     );
