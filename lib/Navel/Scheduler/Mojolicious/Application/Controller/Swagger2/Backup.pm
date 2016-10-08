b92316a06ed3ef6418058f1c116fbc45d624890c
le-garff-yoann <pe.weeble@yahoo.fr>
2016-10-09 01:07:04 +0200

    publisher parser was removed ...

--- lib/Navel/Scheduler/Mojolicious/Application/Controller/Swagger2/Backup.pm
+++ lib/Navel/Scheduler/Mojolicious/Application/Controller/Swagger2/Backup.pm
@@ -19,7 +19,6 @@ sub save_all_configuration {
     $controller->SUPER::save_all_configuration(
         @_,
         $controller->daemon()->{core}->{collectors}->async_write(),
-        $controller->daemon()->{core}->{publishers}->async_write(),
         $controller->daemon()->{core}->{meta}->async_write()
     );
 }
