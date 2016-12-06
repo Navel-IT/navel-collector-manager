ab6b85a1ce41819b7661d8fa36d57df84da27fcd
le-garff-yoann <pe.weeble@yahoo.fr>
2016-12-06 22:28:21 +0100

    use base methods

--- lib/Navel/Scheduler/Mojolicious/Application/Controller/OpenAPI/Collector.pm
+++ lib/Navel/Scheduler/Mojolicious/Application/Controller/OpenAPI/Collector.pm
@@ -18,123 +18,15 @@ use Promises 'collect';
 #-> methods
 
 sub show_associated_queue {
-    my $controller = shift->openapi->valid_input || return;
-
-    my $name = $controller->validation->param('name');
-
-    my $definition = $controller->daemon->{core}->{definitions}->definition_by_name($name);
-
-    return $controller->resource_not_found($name) unless defined $definition;
-
-    $controller->render_later;
-
-    $controller->daemon->{core}->{worker_per_definition}->{$definition->{name}}->rpc(undef, 'queue')->then(
-        sub {
-            $controller->render(
-                openapi => {
-                    amount_of_events => shift
-                },
-                status => 200
-            );
-        }
-    )->catch(
-        sub {
-            $controller->render(
-                openapi => $controller->ok_ko(
-                    [],
-                    [
-                        $definition->full_name . ': ' . (@_ ? join ', ', @_ : 'unexpected error') . '.'
-                    ]
-                ),
-                status => 500
-            );
-        }
-    );
+    shift->_show_associated_queue('queue');
 }
 
 sub delete_all_events_from_associated_queue {
-    my $controller = shift->openapi->valid_input || return;
-
-    my $name = $controller->validation->param('name');
-
-    my $definition = $controller->daemon->{core}->{definitions}->definition_by_name($name);
-
-    return $controller->resource_not_found($name) unless defined $definition;
-
-    $controller->render_later;
-
-    my (@ok, @ko);
-
-    $controller->daemon->{core}->{worker_per_definition}->{$definition->{name}}->rpc(undef, 'dequeue')->then(
-        sub {
-            push @ok, $definition->full_name . ': queue cleared.';
-        }
-    )->catch(
-        sub {
-            push @ko, $definition->full_name . ': ' . (@_ ? join ', ', @_ : 'unexpected error') . '.';
-        }
-    )->finally(
-        sub {
-            $controller->render(
-                openapi => $controller->ok_ko(\@ok, \@ko),
-                status => @ko ? 500 : 200
-            );
-        }
-    );
+    shift->_show_associated_pubsub_connection_status('publisher_backend');
 }
 
 sub show_associated_publisher_connection_status {
-    my $controller = shift->openapi->valid_input || return;
-
-    my $name = $controller->validation->param('name');
-
-    my $definition = $controller->daemon->{core}->{definitions}->definition_by_name($name);
-
-    return $controller->resource_not_found($name) unless defined $definition;
-
-    my $worker_worker = $controller->daemon->{core}->{worker_per_definition}->{$definition->{name}};
-
-    $controller->render_later;
-
-    my $connectable;
-
-    $worker_worker->rpc($definition->{publisher_backend}, 'is_connectable')->then(
-        sub {
-            collect(
-                $worker_worker->rpc($definition->{publisher_backend}, 'is_connecting'),
-                $worker_worker->rpc($definition->{publisher_backend}, 'is_connected'),
-                $worker_worker->rpc($definition->{publisher_backend}, 'is_disconnecting'),
-                $worker_worker->rpc($definition->{publisher_backend}, 'is_disconnected')
-            ) if $connectable = shift;
-        }
-    )->then(
-        sub {
-            my %status;
-
-            ($status{connecting}, $status{connected}, $status{disconnecting}, $status{disconnected}) = @_;
-
-            $status{$_} = $status{$_}->[0] ? 1 : 0 for keys %status;
-
-            $status{connectable} = $connectable ? 1 : 0;
-
-            $controller->render(
-                openapi => \%status,
-                status => 200
-            );
-        }
-    )->catch(
-        sub {
-            $controller->render(
-                openapi => $controller->ok_ko(
-                    [],
-                    [
-                        $definition->full_name . ': ' . (@_ ? join ', ', @_ : 'unexpected error') . '.'
-                    ]
-                ),
-                status => 500
-            );
-        }
-    );
+    shift->_show_associated_pubsub_connection_status('publisher_backend');
 }
 
 # sub AUTOLOAD {}
